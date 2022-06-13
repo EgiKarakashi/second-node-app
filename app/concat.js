9fa750d6330d24d1cecdd57701ca3f983ad725b4
EgiKarakashi <egikarakashi@outlook.com>
2022-06-13 12:53:52 +0200

    Added Sum and Fixed Concat

--- app/concat.js
+++ app/concat.js
@@ -2,10 +2,15 @@ const _ = require('lodash')
 
 const fs = require('fs')
 
-let numbersJson = fs.readFileSync('array.json')
+let numbersFromJson = fs.readFileSync('array.json')
+
+let numbersJson = JSON.parse(numbersFromJson)
+
+const numbers = numbersJson.number
+
 
 function concat(arr) {
-    return _.concat(arr, numbersJson)
+    return _.concat(arr, numbers)
 }
 
 module.exports.concat = concat
