55257472a2581f7b969345e16a17be847728dfed
EgiKarakashi <egikarakashi@outlook.com>
2022-06-13 14:35:55 +0200

    Changes to index file

--- app/index.js
+++ app/index.js
@@ -7,5 +7,5 @@ const numbersToAdd = [1, 2, 3, 4, 5]
 const result = concat.concat(numbersToConcat)
 const result2 = sum.sum(numbersToAdd)
 
-console.log(`The result of the concat is ${result}`)
-console.log(`The result of the sum is ${result2}`)
+console.log(`The result of the concat is: [ ${result} ]`)
+console.log(`The result of the sum is: ${result2}`)
